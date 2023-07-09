b3f178580bdd705a7344bab09a83fd4c4e65e8c0
Shawn Hsu <ambersun1019.shawn@gmail.com>
2023-07-09 16:57:04 +0800

    Update User table csv data

--- simple-rest-lab/load.sql
+++ simple-rest-lab/load.sql
@@ -3,5 +3,4 @@ USE restdb;
 LOAD DATA LOCAL INFILE "/user.csv" INTO TABLE User 
         FIELDS TERMINATED BY "," 
         LINES TERMINATED by "\n" 
-        IGNORE 1 LINES 
-        (username);
+        (id, username, created_at)
