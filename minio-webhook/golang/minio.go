c3f8a6006f2a962cc79645fc0cb5d3855b02d208
Shawn Hsu <ambersun1019.shawn@gmail.com>
2024-10-13 20:55:44 +0800

    Add lifecycle test go code

--- minio-webhook/golang/minio.go
+++ minio-webhook/golang/minio.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"fmt"
 
+	// "os"
+
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
+
+	// "github.com/minio/minio-go/v7/pkg/lifecycle"
 	"github.com/minio/minio-go/v7/pkg/notification"
 )
 
@@ -32,6 +36,45 @@ func main() {
 	cfg.AddQueue(queueConfig)
 
 	bucketName := "mybucket"
-	fmt.Printf("creating minio bucket... %v\n", minioClient.MakeBucket(context.Background(), bucketName, minio.MakeBucketOptions{}))
-	fmt.Printf("registering webhook... %v\n", minioClient.SetBucketNotification(context.Background(), bucketName, cfg))
+	e1 := minioClient.MakeBucket(context.Background(), bucketName, minio.MakeBucketOptions{
+		ObjectLocking: true,
+	})
+	fmt.Printf("creating minio bucket... %v\n", e1)
+	e2 := minioClient.SetBucketNotification(context.Background(), bucketName, cfg)
+	fmt.Printf("registering webhook... %v\n", e2)
+
+	// // test lifecycle hook
+	// file, err := os.Open("go.mod")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
+
+	// fileinfo, err := file.Stat()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// ee := minioClient.SetBucketLifecycle(context.Background(), bucketName, &lifecycle.Configuration{
+	// 	Rules: []lifecycle.Rule{
+	// 		{
+	// 			ID:     "expire",
+	// 			Status: "Enabled",
+	// 			Expiration: lifecycle.Expiration{
+	// 				Days: 1,
+	// 			},
+	// 		},
+	// 	},
+	// })
+	// fmt.Printf("setting bucket lifecycle... %v\n", ee)
+
+	// _, e := minioClient.PutObject(
+	// 	context.Background(),
+	// 	bucketName,
+	// 	"test",
+	// 	file,
+	// 	fileinfo.Size(),
+	// 	minio.PutObjectOptions{},
+	// )
+	// fmt.Printf("uploading file to minio... %v\n", e)
 }
