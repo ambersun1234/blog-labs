124cb69a979096563a4e242f67397518f4948ff7
Shawn Hsu <ambersun1019.shawn@gmail.com>
2026-03-23 18:12:42 +0800

    Update rabbitmq implementation to have qos
    Signed-off-by: Shawn Hsu <ambersun1019.shawn@gmail.com>

--- message-queue/rabbitmq/message-queue.go
+++ message-queue/rabbitmq/message-queue.go
@@ -10,52 +10,68 @@ import (
 )
 
 func publishToMessageQueue(ch *amqp.Channel) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
 
 	counter := 1
-	for {
-		body := fmt.Sprintf("Hello World %v!", counter)
-		err := ch.PublishWithContext(ctx, "", "test", false, false, amqp.Publishing{ContentType: "text/plain", Body: []byte(body)})
-		if err != nil {
-			log.Panic("Failed to publish message")
+	for i := 0; i < 1000; i++ {
+		body := fmt.Sprintf("Hello World (iter %v) %v!", counter, i)
+		if err := ch.PublishWithContext(context.Background(), "", "test", false, false, amqp.Publishing{ContentType: "text/plain", Body: []byte(body)}); err != nil {
+			log.Panic("Failed to publish message", err)
 		}
-		time.Sleep(1 * time.Second)
-		counter += 1
 	}
 }
 
-func consumeFromMessageQueue(ch *amqp.Channel) {
-	queue, err := ch.Consume("test", "", true, false, false, false, nil)
+func consumeSingleFromMessageQueue(ch *amqp.Channel) {
+	queue, err := ch.Consume("test", "", false, false, false, false, nil)
 	if err != nil {
-		log.Panic("Failed to consume from queue")
+		log.Panic("Failed to consume from queue", err)
 	}
 
 	for msg := range queue {
 		log.Printf("Received message: %s", msg.Body)
+		msg.Ack(false)
+		break
+	}
+}
+
+func consumeFromMessageQueue(ch *amqp.Channel) {
+	queue, err := ch.Consume("test", "", false, false, false, false, nil)
+	if err != nil {
+		log.Panic("Failed to consume from queue", err)
+	}
+
+	for msg := range queue {
+		log.Printf("Received message: %s", msg.Body)
+		msg.Ack(false)
 	}
 }
 
 func main() {
 	conn, err := amqp.Dial("amqp://rabbitmq:rabbitmq@localhost:5672/")
 	if err != nil {
-		log.Panic("Failed to connect to RabbitMQ")
+		log.Panic("Failed to connect to RabbitMQ", err)
 	}
 	defer conn.Close()
 
 	ch, err := conn.Channel()
 	if err != nil {
-		log.Panic("Failed to open a channel")
+		log.Panic("Failed to open a channel", err)
 	}
 	defer ch.Close()
+	if err := ch.Qos(100, 0, false); err != nil {
+		log.Panic("Failed to set QoS", err)
+	}
 
 	_, err = ch.QueueDeclare("test", false, false, false, false, nil)
 	if err != nil {
-		log.Panic("Failed to declare queue")
+		log.Panic("Failed to declare queue", err)
 	}
 
 	go publishToMessageQueue(ch)
-	go consumeFromMessageQueue(ch)
+	time.Sleep(10 * time.Second)
+	go consumeSingleFromMessageQueue(ch)
+	// go consumeFromMessageQueue(ch)
 
 	select {}
 }
